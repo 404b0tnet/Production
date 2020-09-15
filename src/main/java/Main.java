c633e72811953060102418e3009ce43849c61183
Justin Smith <jsmith728.js@gmail.com>
2020-09-15 00:04:00 -0400

    Creation of JavaFX form step 2 from https://docs.oracle.com/javafx/2/get_started/form.htm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -20,9 +26,77 @@ public class Main extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("JavaFX Welcome");
 
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+    // GridPane
+    GridPane grid = new GridPane();
+    grid.setAlignment(Pos.CENTER);
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(25, 25, 25, 25));
+
+
+    // Size of application
+    Scene scene = new Scene(grid, 300, 275);
+    primaryStage.setScene(scene);
+
+
+    // Display application
+    primaryStage.show();
+
+
+    // Event button
+    Button btn = new Button("Sign in");
+    HBox hbBtn = new HBox(10);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    hbBtn.getChildren().add(btn);
+    grid.add(hbBtn, 1, 4);
+
+
+    //Text control for displaying the message after button press
+    final Text actiontarget = new Text();
+    grid.add(actiontarget, 1, 6);
+
+
+    // Code to handle an event
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+      @Override
+      public void handle(ActionEvent e) {
+        actiontarget.setFill(Color.FIREBRICK);
+        actiontarget.setText("Sign in button pressed");
+      }
+    });
+
+
+    // Creates a title called "Welcome"
+    Text scenetitle = new Text("Welcome");
+    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+    grid.add(scenetitle, 0, 0, 2, 1);
+
+
+    // Creates a username input field
+    Label userName = new Label("User Name:");
+    grid.add(userName, 0, 1);
+
+    TextField userTextField = new TextField();
+    grid.add(userTextField, 1, 1);
+
+
+    // Creates a password input field
+    Label pw = new Label("Password:");
+    grid.add(pw, 0, 2);
+
+    PasswordField pwBox = new PasswordField();
+    grid.add(pwBox, 1, 2);
+
+  }
+}
+
+/*
+    //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
     Scene scene = new Scene(root, 300, 275);
 
@@ -53,4 +127,5 @@ public class Main extends Application {
     PasswordField pwBox = new PasswordField();
     grid.add(pwBox, 1, 2);
   }
-}
+
+ */
